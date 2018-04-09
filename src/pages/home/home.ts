6aace690b15efb2cedca2f87864859ef6bb49cc4
gcambara <guillermocambara@gmail.com>
2018-04-09 22:24:54 +0200

    Adding Home files

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+//import { IonicPage, NavController, NavParams } from 'ionic-angular'; //gcambara: hid this to avoid lint error, guess I'll need it in the future.
+import { Facebook } from '@ionic-native/facebook';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,43 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  user: any = {};
+  showUser: boolean = false;
 
+  constructor(
+    
+    //gcambara: hid this to avoid lint error, guess I'll need it in the future.
+    //private navCtrl: NavController,
+    //private navParams: NavParams,
+    private facebook: Facebook
+   ) {}
+
+  ionViewDidLoad(){
+    console.log('ionViewDidLoad HomePage');
   }
 
+  loginFacebook(){
+    this.facebook.login(['public_profile', 'email'])
+    .then(rta => {
+      console.log(rta.status);
+      if (rta.status == 'connected'){
+        this.getInfo();
+      };
+    })
+    .catch(error =>{
+      console.error(error);
+    });
+  }
+
+  getInfo(){
+    this.facebook.api('/me?fields=id,name,email,first_name,picture,last_name,gender',['public_profile','email'])
+    .then(data=>{
+      console.log(data);
+      this.showUser = true;
+      this.user = data;
+    })
+    .catch(error =>{
+      console.error(error);
+    });
+  }
 }
