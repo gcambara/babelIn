41a1440e030e67ae84097f45b3f8842ae3e98a56
gcambara <guillermocambara@gmail.com>
2018-04-10 20:01:08 +0200

    Removed login functionality from HomePage

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
-//import { IonicPage, NavController, NavParams } from 'ionic-angular'; //gcambara: hid this to avoid lint error, guess I'll need it in the future.
-import { Facebook } from '@ionic-native/facebook';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -12,39 +11,11 @@ export class HomePage {
   showUser: boolean = false;
 
   constructor(
-    
-    //gcambara: hid this to avoid lint error, guess I'll need it in the future.
-    //private navCtrl: NavController,
-    //private navParams: NavParams,
-    private facebook: Facebook
+    private navCtrl: NavController,
+    private navParams: NavParams,
    ) {}
 
   ionViewDidLoad(){
     console.log('ionViewDidLoad HomePage');
-  }
-
-  loginFacebook(){
-    this.facebook.login(['public_profile', 'email'])
-    .then(rta => {
-      console.log(rta.status);
-      if (rta.status == 'connected'){
-        this.getInfo();
-      };
-    })
-    .catch(error =>{
-      console.error(error);
-    });
-  }
-
-  getInfo(){
-    this.facebook.api('/me?fields=id,name,email,first_name,picture,last_name,gender',['public_profile','email'])
-    .then(data=>{
-      console.log(data);
-      this.showUser = true;
-      this.user = data;
-    })
-    .catch(error =>{
-      console.error(error);
-    });
   }
 }
